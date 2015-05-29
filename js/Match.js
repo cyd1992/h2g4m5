dd555e931341beb5c7d186f08add3f927b03253d
cyd1992 <315309286@qq.com>
2015-05-29 21:50:59 +0800

    3
    match

--- js/Match.js
+++ js/Match.js
@@ -9,7 +9,7 @@ function Match(x,y)
 	this.rotation = 0;
 	this.drawMatch = function(){
 		this.graphics.beginFill("#D19835");
-		this.graphics.drawRect(-200,-200,400,400);
+		this.graphics.drawRect(-200,-20,400,40);
 		//this.graphics.endFill();	
 		//alert('x:'+this.x);
 	};
@@ -17,18 +17,25 @@ function Match(x,y)
 	// in this case that means it executes in the scope of the button.
 	this.on("mousedown", function (evt) {
 		//this.parent.addChild(this);
-		if(this.touch_count==0){
+		// if(this.touch_count==0){
 			this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
-			this.x1 = evt.stageX;
-			this.y1 = evt.stageY;
-			this.touch_count ++;
-		}else if(this.touch_count==1)
+		// 	this.x1 = evt.stageX;
+		// 	this.y1 = evt.stageY;
+		// 	this.touch_count ++;
+		// }else if(this.touch_count==1)
+		// {
+		// 	this.x2 = evt.stageX;
+		// 	this.y2 = evt.stageY; 
+		// 	this.touch_count ++;
+		// }
+
+		this.dist = Math.abs(evt.localX) +Math.abs(evt.localY);
+		if(this.dist<120)
 		{
-			this.x2 = evt.stageX;
-			this.y2 = evt.stageY; 
-			this.touch_count ++;
-		}
-		this.parent.getChildByName("Timer").text = this.touch_count + " x:"+evt.localX+" y:"+evt.localY;
+			this.touch_count = 1;
+		}else this.touch_count = 2;
+
+		this.parent.getChildByName("Timer").text = this.touch_count + " x:"+evt.stageX+" y:"+evt.stageY;
 		update = true;
 	});
 
@@ -39,24 +46,22 @@ function Match(x,y)
 			this.x = evt.stageX + this.offset.x;
 			this.y = evt.stageY + this.offset.y;
 			// indicate that the stage should be updated on the next tick:
-			this.parent.getChildByName("Timer").text = this.touch_count+"  offset:"+evt.stageY;
+			this.parent.getChildByName("Timer").text = this.touch_count+" x:"+this.x+" y:"+this.y;
 			update = true;
 		}else if(this.touch_count==2)
 		{
-			if(Math.abs(this.y1 - evt.stageY)<10)
-			{
-				this.y1 = evt.stageY;
-				this.x1 = evt.stageX;
-			}else if(Math.abs(this.y2 - evt.stageY)<10)
-			{
-				this.y2 = evt.stageY;
-				this.x2 = evt.stageX;
-			}
-
-			var temp =  (this.y2 -this.y1)/Math.sqrt(Math.pow((this.y2 - this.y1),2) + Math.pow((this.x2 - this.x1),2));
-			//alert('anger:'+ Math.asin(temp)*180/Math.PI);
-			this.rotation = Math.asin(temp)*180/Math.PI;
-			this.parent.getChildByName("Timer").text = this.touch_count+"  anger:"+Math.acos(temp)*180/Math.PI;
+			 var x_t = evt.stageX + this.offset.x;
+			 var y_t = evt.stageY + this.offset.y;
+			
+			// var temp =  (this.y2 -this.y1)/Math.sqrt(Math.pow((this.y2 - this.y1),2) + Math.pow((this.x2 - this.x1),2));
+			// var temp =  (y_t - this.y)/Math.sqrt(Math.pow((y_t - this.y),2) + Math.pow((x_t - this.x),2));
+			var temp =  (evt.stageY - this.y)/Math.sqrt(Math.pow((evt.stageY - this.y),2) + Math.pow((evt.stageX - this.x),2));
+			if((evt.stageX - this.x)>0)
+			{				
+				this.rotation = Math.asin(temp)*180/Math.PI;
+			}else  this.rotation = 180 - Math.asin(temp)*180/Math.PI;
+			
+			this.parent.getChildByName("Timer").text = this.touch_count+"  x:"+evt.stageX+" y:"+evt.stageY +" x0:"+this.x+" y0:"+this.y+"  anger:"+this.rotation;
 			update = true;
 		}
 
@@ -64,7 +69,7 @@ function Match(x,y)
 
 	// the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
 	this.on("pressup", function (evt) {
-		this.touch_count --;
+		this.touch_count =0;
 		this.parent.getChildByName("Timer").text = this.touch_count;
 		update = true;
 		// indicate that the stage should be updated on the next tick:
